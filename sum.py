f081a05c952467b4805bce7fe802fb779daadcc6
AmalSankar009 <89658953+AmalSankar009@users.noreply.github.com>
2023-02-02 11:39:49 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,7 @@
+a = input('Enter first number: ')
+b = input('Enter second number: ')
+s = float(a) + float(b)
+print('The sum is',(s))
+
+
+print('The sum is %.1f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
